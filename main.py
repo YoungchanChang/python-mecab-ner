34b10486de856aa86da68644d574423191ac2ca5
youngchan <san0558@naver.com>
2022-02-26 19:29:46 +0900

    Update main.py
    - main.py에서 실행

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
 from pathlib import Path
 
-from mecab_reader import MecabDataReader
+from mecab_reader import MecabDataReader, MecabDataWriter
 from python_mecab_ner import MecabParser
 
 if __name__ == "__main__":
 
-        test_sentence = "나는 서울대병원에 갔어"
-
-        mecab_parse_results = list(
-            MecabParser(test_sentence).gen_mecab_token_feature())
-
-        for idx, mecab_parse_item in enumerate(mecab_parse_results):
-            print(mecab_parse_item)
-
-        mecab_parse_results = list(
-            MecabParser(test_sentence).gen_mecab_compound_token_feature())
-
-        for idx, mecab_parse_item in enumerate(mecab_parse_results):
-            print(mecab_parse_item)
-
-        storage_path = "./python_mecab_ner/data"
-
-        # clear data
-        m_g = MecabDataReader(ner_path=storage_path)
-
-        for data_item in m_g.gen_all_mecab_category_data(m_g.ner_path, use_mecab_parser=True):
-            header, content = data_item
-            print(header)
-            print(content)
-
-            #
-            # assert isinstance(large_category, str)
-            # assert isinstance(medium_category, str)
-            # assert isinstance(data_dict, dict)
-
-
+    m_d_w = MecabDataWriter("./test_data")
+    m_d_w.write_category()
