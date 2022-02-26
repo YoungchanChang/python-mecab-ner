79931868910cc42fc0af0e9939cd95c7b3c01b96
youngchan <san0558@naver.com>
2022-02-26 22:29:14 +0900

    Update setup.py
    - version update

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup, find_packages
-
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-
 setup(
     name             = 'python-mecab-ner',
-    version          = '0.0.3',
+    version          = '0.0.5',
     description      = 'Test package for distribution',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -17,7 +15,6 @@ setup(
     url              = 'https://github.com/YoungchanChang/python-mecab-ner',
     download_url     = '',
     install_requires = ['pybind11 ~= 2.0', "python-mecab-ko ~= 1.0.12"],
-	include_package_data=True,
 	packages=find_packages(),
     keywords         = ['Text Processing', 'Text Processing :: Linguistic', 'NER', "mecab", "mecab-ko", "mecab-ner"],
     python_requires  = '>=3.7',
@@ -29,6 +26,6 @@ setup(
         'Intended Audience :: Science/Research',
         'Natural Language :: Korean',
         "Operating System :: OS Independent",
-    ]
+    ],
 )
 
