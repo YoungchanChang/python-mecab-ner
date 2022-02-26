78daa8bf2f08e6c384cd396d0f902d1bfcecb4e7
youngchan <san0558@naver.com>
2022-02-26 17:06:53 +0900

    Add mecab_ner_dir in conftest.py
    - 메캅 ner디렉터리 정보 conftest 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,17 @@
 import pytest
+from pathlib import Path
 
 
 @pytest.fixture(scope="function")
 def mock_mecab_parser_sentence():
     data = {"compound_count_2" : "나는 서울대병원에 갔어"}
     return data
+
+
+@pytest.fixture(scope="function")
+def mecab_ner_dir():
+    python_mecab_ner_dir = Path(__file__).resolve().parent.parent.joinpath("python_mecab_ner", "data", "ner_data")
+    data = {"python_mecab_ner": python_mecab_ner_dir}
+    return data
+
+
