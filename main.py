edaadea413e41f2704f34c781f734c2471567089
youngchan <san0558@naver.com>
2022-02-28 19:46:00 +0900

    Update main.py
    - 메인 코드 업데이트

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-from mecab_reader import MecabDataReader, MecabDataWriter
-from python_mecab_ner import MecabParser
+from mecab_ner import MecabNer
 
+# 가봉에 가서 감이 먹고싶네
 if __name__ == "__main__":
+    python_mecab_ner_dir = Path(__file__).resolve().parent.joinpath("python_mecab_ner", "data", "ner_data")
+    m_n = MecabNer(str(python_mecab_ner_dir), infer=False)
 
-    m_d_w = MecabDataWriter("./test_data")
-    m_d_w.write_category()
+    test_sentence = "금요일에 만나요를 들을래"
+    print(m_n.parse(test_sentence))
+    print(m_n.morphs(test_sentence))
+    print(m_n.ners(test_sentence))
