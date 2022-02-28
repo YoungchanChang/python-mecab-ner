bcefc6c5cae3c112a70e8c4d9d5b274b06eb28e2
youngchan <san0558@naver.com>
2022-02-28 19:45:26 +0900

    Add test_infer_mecab_ner_feature in test_mecab_ner.py
    - 테스트 데이터 코드 추가

--- tests/test_mecab_ner.py
+++ tests/test_mecab_ner.py
@@ -1,48 +1,138 @@
 from pathlib import Path
 
-from mecab_ner import MecabNer
+from mecab_ner import MecabNer, gen_integrated_entity
 from service.mecab_parser import MecabParser
 
 
 
-def test_mecab_ner_current_dir():
+def test_mecab_ner_simple():
     """
     디렉터리 추가 후 데이터 테스트 추가
+    1. 기본 기능 테스트
+    """
+    m_n = MecabNer()
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+    parse_result = m_n.parse(test_sentence)
+    assert len(parse_result) == 10
+
+    morphs_result = m_n.morphs(test_sentence)
+    assert morphs_result == ['아이유', '의', '금요일에 만나요', '를', '듣', '으면서', '신촌 딸기', '를', '먹', '을래']
+
+    ners_result = m_n.ners(test_sentence)
+    assert ners_result == [('아이유', 'ner_example_music_singer', '가수'), ('금요일에 만나요', 'ner_example_music_song', '노래'), ('신촌 딸기', 'ner_example_fruit', '과일')]
+
+
+def test_mecab_ner_search_category():
+    """
+    디렉터리 추가 후 데이터 테스트 추가
+    2. search_category테스트
+    """
+    m_n = MecabNer(search_category=["ner_example_music_singer"])
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+    parse_result = m_n.parse(test_sentence)
+    assert len(parse_result) == 15
+
+    morphs_result = m_n.morphs(test_sentence)
+    assert morphs_result == ['아이유', '의', '금', '요일', '에', '만나', '요', '를', '듣', '으면서', '신촌', '딸기', '를', '먹', '을래']
+
+    ners_result = m_n.ners(test_sentence)
+    assert ners_result == [('아이유', 'ner_example_music_singer', '가수')]
+
+    m_n = MecabNer(search_category=["ner_example_music_singer", "ner_example_fruit"])
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+    parse_result = m_n.parse(test_sentence)
+    assert len(parse_result) == 14
+
+    morphs_result = m_n.morphs(test_sentence)
+    assert morphs_result == ['아이유', '의', '금', '요일', '에', '만나', '요', '를', '듣', '으면서', '신촌 딸기', '를', '먹', '을래']
+
+    ners_result = m_n.ners(test_sentence)
+    assert ners_result == [('아이유', 'ner_example_music_singer', '가수'), ('신촌 딸기', 'ner_example_fruit', '과일')]
+
+
+def test_mecab_ner_infer():
+    """
+    디렉터리 추가 후 데이터 테스트 추가
+    3. 추론 기능 테스트
+    """
+    m_n = MecabNer(infer=False)
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+    parse_result = m_n.parse(test_sentence)
+    assert len(parse_result) == 11
+
+    morphs_result = m_n.morphs(test_sentence)
+    assert morphs_result == ['아이유', '의', '금요일에 만나요', '를', '듣', '으면서', '신촌', '딸기', '를', '먹', '을래']
+
+    ners_result = m_n.ners(test_sentence)
+    assert ners_result == [('아이유', 'ner_example_music_singer', '가수'), ('금요일에 만나요', 'ner_example_music_song', '노래'), ('딸기', 'ner_example_fruit', '과일')]
+
+    m_n = MecabNer(search_category=["ner_example_music_singer", "ner_example_fruit"], infer=False)
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+    parse_result = m_n.parse(test_sentence)
+    assert len(parse_result) == 15
+
+    morphs_result = m_n.morphs(test_sentence)
+    assert morphs_result == ['아이유', '의', '금', '요일', '에', '만나', '요', '를', '듣', '으면서', '신촌', '딸기', '를', '먹', '을래']
+
+    ners_result = m_n.ners(test_sentence)
+    assert ners_result == [('아이유', 'ner_example_music_singer', '가수'), ('딸기', 'ner_example_fruit', '과일')]
+
+
+def test_get_category_entity():
+    """
+    순수하게 엔티티만 가져오는 기능 테스트
     """
 
-    # 테스트 데이터 추가
-    ner_dir = Path(__file__).resolve().parent.parent
-    m_d_w = MecabDataWriter(ner_path=str(ner_dir), clear_mecab_dir=True)
-    m_d_w.write_category()
-
-
-    # 카테고리 리스트에 따라 필터링
-    category_list = ["test_mecab_data",]
-    m_n = MecabNer(search_category=category_list)
-    for entity_item in m_n.get_category_entity("프룬이 먹고 싶어"):
-        if entity_item.category.large == "test_mecab_data":
-            assert entity_item.entity == "프룬"
-
-    # 테스트 데이터 제거, 원래 테스트 데이터 사용
-    python_mecab_ner_dir = Path(__file__).resolve().parent.parent.joinpath("python_mecab_ner", "data", "ner_data")
-    m_d_w = MecabDataWriter(str(python_mecab_ner_dir), clear_mecab_dir=True)
-    m_d_w.write_category()
-
-
-
-
-def test_mecab_data_write(mecab_ner_dir):
-
-    """
-    ner_data를 읽은 뒤, mecab_data로 전환하는 테스트 코드
-    - ner_data에서 읽었을 때의 개수와, mecab_data에서 읽었을 때의 개수가 같아야 한다.
-    """
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
 
     m_n = MecabNer()
-    for entity_item in m_n.get_category_entity("가봉에 가서 감이 먹고싶네"):
-        print(entity_item)
+    m_n.mecab_parsed_list = list(MecabParser(sentence=test_sentence).gen_mecab_compound_token_feature())
 
-# location데이터 => small 카테고리가 여러개
-# body데이터 => "이"가 들어가있음
-# fastfood 처음부터 #이 들어가 있지 않음
-# song데이터 영어가 들어가 있음
+
+    mecab_category_list = list(m_n.get_category_entity())
+
+    assert mecab_category_list[0].entity == "아이유"
+    assert mecab_category_list[0].category.large == "ner_example_music_singer"
+    assert mecab_category_list[0].category.small == "#가수"
+
+    assert mecab_category_list[1].entity == "딸기"
+    assert mecab_category_list[1].category.large == "ner_example_fruit"
+    assert mecab_category_list[1].category.small == "#과일"
+
+    assert mecab_category_list[2].entity == "금요일에 만나요"
+    assert mecab_category_list[2].category.large == "ner_example_music_song"
+    assert mecab_category_list[2].category.small == "#노래"
+
+
+def test_infer_mecab_ner_feature():
+    """
+    엔티티에서 데이터 추론하는 기능 테스트
+    """
+
+    test_sentence = "아이유의 금요일에 만나요를 들으면서 신촌 딸기를 먹을래"
+
+    m_n = MecabNer()
+    m_n.mecab_parsed_list = list(MecabParser(sentence=test_sentence).gen_mecab_compound_token_feature())
+
+
+    mecab_category_list = list(m_n.gen_infer_mecab_ner_feature())
+
+    assert mecab_category_list[0].word == "아이유"
+    assert mecab_category_list[0].category.large == "ner_example_music_singer"
+    assert mecab_category_list[0].category.small == "가수"
+
+    assert mecab_category_list[1].word == "금요일에 만나요"
+    assert mecab_category_list[1].category.large == "ner_example_music_song"
+    assert mecab_category_list[1].category.small == "노래"
+
+    assert mecab_category_list[2].word == "신촌 딸기"
+    assert mecab_category_list[2].category.large == "ner_example_fruit"
+    assert mecab_category_list[2].category.small == "과일"
+
+    many_entity_index_list = m_n.fill_entity_in_blank(mecab_category_list)
+
+    assert many_entity_index_list == [1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0]
+
+    z = list(gen_integrated_entity(many_entity_index_list))
+
+    assert z == [(0, 0), (2, 6), (10, 11)]
