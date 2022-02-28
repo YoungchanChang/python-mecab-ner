6cc2179881dfdf87f55c6b1460f9603995c27175
youngchan <san0558@naver.com>
2022-02-28 09:32:08 +0900

    Update mecab_ner.py
    - 메캅 엔티티 분석기 추가

--- python_mecab_ner/mecab_ner.py
+++ python_mecab_ner/mecab_ner.py
@@ -1,5 +1,5 @@
 import copy
-from typing import List, Generator
+from typing import List, Generator, Iterable
 from pathlib import Path
 
 from mecab_parser import MecabParser
@@ -7,12 +7,26 @@ from mecab_storage import MeCabStorage
 from mecab_reader import MecabDataController
 from domain.mecab_domain import MecabWordCategory, Category, MecabPatternData, MecabNerFeature, NerFeature
 
-MECAB_WORD_FEATURE = 0
+MECAB_READING_WORD = 0
+MECAB_FEATURE = 1
+WORD_IDX = 0
 INFER_FORWARD = 1
 INFER_BACKWARD = 2
+FULL_WORD = 1
+START_IDX = 0
+END_IDX = 1
+EMPTY_WORD = 0
 
 
-def find_patterns_idx(pattern, find_tokens: List, parse_character=False) -> List:
+def find_patterns_idx(pattern, target_tokens: List, parse_character=False) -> List:
+
+    """
+    리스트에서 단어 패턴을 찾아서 반환하는 함수
+    :param pattern: 찾고자 하는 패턴
+    :param target_tokens: 찾아야 하는 토큰 리스트
+    :param parse_character: 캐릭터 단위로 분해
+    :return: 저장되어 있는 리스트 반환
+    """
 
     if parse_character:
         pattern = list(pattern)
@@ -22,9 +36,9 @@ def find_patterns_idx(pattern, find_tokens: List, parse_character=False) -> List
 
     tmp_save_list = []
 
-    for i in range(len(find_tokens)-len(pattern)+1):
+    for i in range(len(target_tokens) - len(pattern) + 1):
         for j in range(len(pattern)):
-            if find_tokens[i+j][MECAB_WORD_FEATURE] != pattern[j]:
+            if target_tokens[i + j][MECAB_READING_WORD] != pattern[j]:
                 break
         else:
             tmp_save_list.append((i, i+len(pattern)))
@@ -43,7 +57,7 @@ def gen_integrated_entity(blank_list: List) -> Generator:
     end_idx = None
     is_word_start = False
     for idx, item in enumerate(blank_list):
-        if item == MecabNer.FULL_WORD: # 1이 채워진 단어에 대해서 수행
+        if item == FULL_WORD: # 1이 채워진 단어에 대해서 수행
             end_idx = idx # 끝단어를 시작점으로 잡는다.
 
             if is_word_start is False: # 1로 채워진 단어의 시작이라면, 시작점을 현 인덱스로 저장한다.
@@ -61,41 +75,64 @@ def gen_integrated_entity(blank_list: List) -> Generator:
             continue
 
 
+def _get_default_mecab_large_cat(mecab_path):
+
+    """
+    지정한 카테고리 없을 시 메캅 경로를 기본 분석 경로로 지정 위한 메소드
+    :param mecab_path: 메캅 분석된 데이터 저장 경로
+    :return: 저장된 카테고리
+    """
+
+    for path_item in Path(mecab_path).iterdir():
+        if path_item.suffix == ".txt":
+            yield path_item.stem
+
+
+def _prevent_compound_token(pattern_item: List, mc_p_l: List) -> None:
+    """
+    - 한 카테고리에서 같은 짧은 단어 추출하는 경우 방지. ex) 양념 치킨 추출 후 치킨 추출
+    :param pattern_item: 찾는 패턴
+    :param mc_p_l: 문장을 메캅으로 나눈 리스트
+    """
+
+    for pattern_idx_item in range(pattern_item[START_IDX], pattern_item[END_IDX], 1):
+        mc_p_l[pattern_idx_item] = ("*", mc_p_l[pattern_idx_item][MECAB_FEATURE])
+
+
 class MecabNer(MecabDataController):
     """
     엔티티 추출하는 클래스
     - 앞에 단어 품사를 통한 추론 기능
-    - 뒤에 단어 품사를 통한 추론 기능
     - 메캅 형태소 분석 비교 및 형태소와 문자열 비교 기능
     """
 
     MIN_MEANING = 2
-    START_IDX = 0
-    END_IDX = 1
-    ONE_WORD = 1
-    MECAB_FEATURE_IDX = 1
-    WORD_IDX = 0
-    ENTITY = 0
-    EMPTY_WORD = 0
-    FULL_WORD = 1
+    NER_POS = "ner"
     ENTITY_POS_LIST = ["NNG", "NNP", "NNB", "NNBC", "NR", "NP", "XSN", "XR", "SL", "SH", "SN", "UNKNOWN"]
+    INFER_ENTITY_POS_LIST = ["NNG", "NNP"]
 
     def __init__(self, ner_path: str = None, search_category: List = None, infer=True, clear_mecab_dir=True):
+
+        """
+        :param ner_path: ner 경로
+        :param search_category: 찾고자 하는 카테고리
+        :param infer: 추론 여부
+        :param clear_mecab_dir: 기존 디렉터리 삭제 여부
+        """
+
         super().__init__(ner_path=ner_path, clear_mecab_dir=clear_mecab_dir)
         self.search_category = search_category
         self.write_category()
 
         self.mecab_parsed_list = []
+
         if search_category is None:
-            self.search_category = list(self._get_category_list())
+            self.search_category = list(_get_default_mecab_large_cat(mecab_path= self.mecab_path))
+
         self.infer = infer
 
-    def _get_category_list(self):
-        for path_item in Path(self.mecab_path).iterdir():
-            if path_item.suffix == ".txt":
-                yield path_item.stem
+    def get_category_entity(self) -> Iterable[MecabWordCategory]:
 
-    def get_category_entity(self) -> Generator:
         """
         문장에서 카테고리별로 엔티티 추출
         :param sentence: 엔티티를 찾고자 하는 문장
@@ -114,13 +151,10 @@ class MecabNer(MecabDataController):
                     original_data, mecab_data = small_category_item.split(",")
 
                     category_data = Category(large=category, small=small_category)
-                    yield from self.get_pattern(MecabPatternData(category=category_data, dictionary_data=original_data, pattern=mecab_data, sentence=mecab_parsed_copied))
-                    yield from self.get_pattern(MecabPatternData(category=category_data, dictionary_data=original_data,pattern=original_data, sentence=mecab_parsed_copied, min_meaning=2, parse_character=True))
+                    yield from self._get_pattern(MecabPatternData(category=category_data, dictionary_data=original_data, pattern=mecab_data, sentence=mecab_parsed_copied))
+                    yield from self._get_pattern(MecabPatternData(category=category_data, dictionary_data=original_data, pattern=original_data, sentence=mecab_parsed_copied, min_meaning=2, parse_character=True))
 
-
-
-
-    def get_pattern(self, m_p_d: MecabPatternData) -> Generator:
+    def _get_pattern(self, m_p_d: MecabPatternData) -> Iterable[MecabWordCategory]:
         """
         문장에서 데이터 패턴을 찾는 기능
         - 엔티티의 마지막 단어는 명사형이여야 함. (이가 조사임에도 엔티티로 빠지는 경우 방지)
@@ -132,102 +166,116 @@ class MecabNer(MecabDataController):
         if (len(m_p_d.pattern) >= m_p_d.min_meaning) and space_token_contain_pattern:
             for pattern_item in space_token_contain_pattern:
 
-                pattern_end_pos = m_p_d.sentence[pattern_item[self.END_IDX] - 1][self.MECAB_FEATURE_IDX].pos
+                pattern_end_pos = m_p_d.sentence[pattern_item[END_IDX] - 1][MECAB_FEATURE].pos
 
                 if pattern_end_pos not in self.ENTITY_POS_LIST:
                     continue
 
-                self._prevent_compound_token(pattern_item, m_p_d.sentence)
+                _prevent_compound_token(pattern_item, m_p_d.sentence)
 
                 yield MecabWordCategory(category=m_p_d.category,
-                                        start_idx=pattern_item[self.START_IDX],
-                                        end_idx=pattern_item[self.END_IDX], entity=m_p_d.dictionary_data)
+                                        start_idx=pattern_item[START_IDX],
+                                        end_idx=pattern_item[END_IDX], entity=m_p_d.dictionary_data)
 
-    def infer_entity(self, mecab_parsed_list: List, mecab_category_item: MecabWordCategory) -> MecabWordCategory:
-        """ pos에 따라 start_index 변경"""
+    def infer_entity(self, mecab_category_item: MecabWordCategory) -> MecabWordCategory:
+
+        """
+        - mecab_category_item의 앞에 명사형이 올 경우 카테고리 아이템의 인덱스 값 변경
+        :param mecab_category_item:  메캅 카테고리 아이템
+        :return: 앞 단어 형태소에 따라 변경된 카테고리 아이템
+        """
 
         end_point = 0
         if mecab_category_item.start_idx == 1:
             end_point = -1
 
         for idx_range in range(mecab_category_item.start_idx - 1, end_point, -1):
-            if mecab_parsed_list[idx_range][MecabNer.MECAB_FEATURE_IDX].pos in ["NNG", "NNP"]:
-                mecab_category_item.start_idx = mecab_parsed_list[idx_range][1].mecab_token_compound_idx
+            if self.mecab_parsed_list[idx_range][MECAB_FEATURE].pos in self.INFER_ENTITY_POS_LIST:
+                mecab_category_item.start_idx = self.mecab_parsed_list[idx_range][MECAB_FEATURE].mecab_token_compound_idx
                 continue
             break
         return mecab_category_item
 
-    def _prevent_compound_token(self, pattern_item: List, mc_p_l: List) -> None:
-        """
-        - 한 카테고리에서 같은 짧은 단어 추출하는 경우 방지. ex) 양념 치킨 추출 후 치킨 추출
-        :param pattern_item: 찾는 패턴
-        :param mc_p_l: 문장을 메캅으로 나눈 리스트
-        """
-
-        for pattern_idx_item in range(pattern_item[self.START_IDX], pattern_item[self.END_IDX], self.ONE_WORD):
-            mc_p_l[pattern_idx_item] = ("*", mc_p_l[pattern_idx_item][self.MECAB_FEATURE_IDX])
-
 
     def fill_entity_in_blank(self, mecab_entity_category_list: List) -> List:
+
         """
-        문장에서 인덱스 값이 있는 경우
+        문장에서 인덱스 값이 있는 경우 1로 채워서 반환
         :param mecab_entity_category_list:
-        :param length:
-        :return:
+        :return: 메캅 특성의 시작, 끝점이 있는 인덱스는 1로 채워서 반환
         """
-        blank = [MecabNer.EMPTY_WORD] * len(self.mecab_parsed_list)
+
+        blank_list = [EMPTY_WORD] * len(self.mecab_parsed_list)
 
         for mecab_entity_category_item in mecab_entity_category_list:
             for i in range(mecab_entity_category_item.start_idx, mecab_entity_category_item.end_idx, 1):
-                blank[i] = self.FULL_WORD
+                blank_list[i] = FULL_WORD
 
-        return blank
+        return blank_list
 
-    def gen_mecab_category_entity(self):
+    def gen_infer_mecab_ner_feature(self) -> Iterable[MecabNerFeature]:
 
+        """
+        추론 메캅 단어 특성 반환
+        :return: 추론된 단어 정식 형태로로 분석하여 변환
+        """
 
         mecab_entity_category_list = []
         for category_entity_item in self.get_category_entity():
-            mecab_entity_category_list.append(self.infer_entity(self.mecab_parsed_list, category_entity_item))
+            mecab_entity_category_list.append(self.infer_entity(category_entity_item))
 
         many_entity_index_list = self.fill_entity_in_blank(mecab_entity_category_list)
 
         for integrated_entity_item in gen_integrated_entity(many_entity_index_list):
-            end_idx = integrated_entity_item[1] + 1
-            start_idx = integrated_entity_item[0]
+            end_idx = integrated_entity_item[END_IDX] + 1
+            start_idx = integrated_entity_item[START_IDX]
             mecab_parsed_token = self.mecab_parsed_list[start_idx:end_idx]
             restore_tokens = MeCabStorage().reverse_compound_tokens(mecab_parsed_token)
             restore_sentence = " ".join(restore_tokens)
             for entity_category_item in mecab_entity_category_list:
                 if entity_category_item.end_idx == end_idx:
-                    small_category_replace = entity_category_item.category.small.replace("#", "").strip()
+                    small_category_replace = entity_category_item.category.small.replace(MecabDataController.SMALL_CAT_DIVIDER, "").strip()
                     yield MecabNerFeature(word=restore_sentence,
-                                        pos="ner",
+                                        pos=self.NER_POS,
                                         start_idx=start_idx,
                                         end_idx=end_idx,
                                         category=Category(large=entity_category_item.category.large, small=small_category_replace))
 
+
     def parse(self, sentence: str):
+        """
+        문장 분해 후 값 돌려주는 기능
+        :param sentence: 입력 문장
+        :return: 파싱돈 결과
+        """
+
         self.mecab_parsed_list = list(MecabParser(sentence=sentence).gen_mecab_compound_token_feature())
 
-        mecab_cat_list = list(self.gen_mecab_category_entity())
+        mecab_cat_list = list(self.gen_infer_mecab_ner_feature())
         cat_idx_list=[]
         [cat_idx_list.extend(list(range(x.start_idx, x.end_idx, 1))) for x in mecab_cat_list]
 
         parse_result = []
         for idx, mecab_parse_item in enumerate(self.mecab_parsed_list):
+
             for mecab_item in mecab_cat_list:
                 if idx == mecab_item.end_idx:
-                    parse_result.append((mecab_item.word, mecab_item))
+                    parse_result.append((mecab_item.word,
+                                         NerFeature(word=mecab_item.word,
+                                                    pos=mecab_item.pos,
+                                                    category=mecab_item.category)))
                     break
             if idx in cat_idx_list:
                 continue
-            parse_result.append((mecab_parse_item[1].word, NerFeature(word=mecab_parse_item[self.MECAB_FEATURE_IDX].word, pos=mecab_parse_item[self.MECAB_FEATURE_IDX].pos)))
+
+            parse_result.append((mecab_parse_item[MECAB_FEATURE].word, NerFeature(word=mecab_parse_item[MECAB_FEATURE].word,
+                                                                                  pos=mecab_parse_item[MECAB_FEATURE].pos)))
+
         return parse_result
 
     def morphs(self, sentence: str):
-        return [x[self.WORD_IDX] for x in self.parse(sentence=sentence)]
+        return [x[MECAB_READING_WORD] for x in self.parse(sentence=sentence)]
 
     def ners(self, sentence: str):
-        result = [(x[self.MECAB_FEATURE_IDX].word, x[self.MECAB_FEATURE_IDX].category.large, x[self.MECAB_FEATURE_IDX].category.small) for x in self.parse(sentence=sentence) if x[self.MECAB_FEATURE_IDX].pos == "ner"]
+        result = [(x[MECAB_FEATURE].word, x[MECAB_FEATURE].category.large, x[MECAB_FEATURE].category.small) for x in self.parse(sentence=sentence) if x[MECAB_FEATURE].pos == "ner"]
         return result
