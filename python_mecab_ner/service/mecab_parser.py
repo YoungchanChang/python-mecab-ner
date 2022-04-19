93ffa86c268b08454d477cad6f269a3d1b32a18a
youngchan <san0558@naver.com>
2022-04-19 21:49:10 +0900

    Update mecab_parser.py
    - 문장 생성자에서 매개변수로 이동
    - 객체 한 번 생성시 문장 반복해서 삽입 가능

--- python_mecab_ner/service/mecab_parser.py
+++ python_mecab_ner/service/mecab_parser.py
@@ -75,17 +75,15 @@ class MecabParser:
     FIRST_WORD = 0
     type_list = ["Compound", "Inflect"]
 
-    def __init__(self, sentence: str, dicpath=''):
+    def __init__(self, dicpath=''):
         argument = ''
 
         if dicpath != '':
             argument = '-d %s' % dicpath
 
         self.tagger = _mecab.Tagger(argument)
-        self.sentence = sentence
-        self.sentence_token = self.sentence.split()
 
-    def _get_space_token_idx(self, mecab_word_feature: MecabWordFeature) -> int:
+    def _get_space_token_idx(self, sentence: str, mecab_word_feature: MecabWordFeature) -> int:
 
         """
         스페이스로 토큰 분석한 인덱스 값 반환
@@ -93,25 +91,26 @@ class MecabParser:
         :return: 스페이스 토큰 분석한 결과
         """
 
-        for idx_token, sentence_token_item in enumerate(self.sentence_token):
+        sentence_token = sentence.split()
+        for idx_token, sentence_token_item in enumerate(sentence_token):
 
             index_string = sentence_token_item.find(mecab_word_feature.word)
             if index_string != STRING_NOT_FOUND:
 
-                self.sentence_token[idx_token] = delete_pattern_from_string(sentence_token_item, mecab_word_feature.word, index_string)
+                sentence_token[idx_token] = delete_pattern_from_string(sentence_token_item, mecab_word_feature.word, index_string)
 
                 return idx_token
 
         return False
 
-    def gen_mecab_token_feature(self) -> Generator:
+    def gen_mecab_token_feature(self, sentence: str) -> Generator:
 
         """
         메캅으로 형태소 분석한 토큰 제너레이터로 반환
         스페이스로 분석한 토큰의 정보와 형태소로 분석한 토큰의 정보 포함
         """
 
-        lattice = _create_lattice(self.sentence)
+        lattice = _create_lattice(sentence)
 
         if not self.tagger.parse(lattice):
             raise MeCabError(self.tagger.what())
@@ -120,7 +119,7 @@ class MecabParser:
             mecab_token_feature = _get_mecab_feature(mecab_token)
             mecab_token_feature.mecab_token_idx = mecab_token_idx
 
-            space_token_idx = self._get_space_token_idx(mecab_token_feature)
+            space_token_idx = self._get_space_token_idx(sentence, mecab_token_feature)
 
             if space_token_idx is not False:
                 mecab_token_feature.space_token_idx = space_token_idx
@@ -128,13 +127,13 @@ class MecabParser:
 
                 yield mecab_token_feature
 
-    def tokenize_mecab_compound(self) -> Generator:
+    def tokenize_mecab_compound(self, sentence: str) -> Generator:
 
         """
         메캅으로 분석한 토큰 제너레이터로 반환 결과 중에 복합여, 굴절형태소 있는 경우 토큰화
         """
 
-        for compound_include_item in self.gen_mecab_token_feature():
+        for compound_include_item in self.gen_mecab_token_feature(sentence=sentence):
             if compound_include_item.type in self.type_list:
                 compound_item_list = compound_include_item.expression.split("+")
                 for compound_item in compound_item_list:
@@ -147,30 +146,30 @@ class MecabParser:
             else:
                 yield compound_include_item.word, compound_include_item
 
-    def gen_mecab_compound_token_feature(self) -> Generator:
+    def gen_mecab_compound_token_feature(self, sentence: str) -> Generator:
 
         """
-
         :return: 복합어를 분해한 메캅 토큰 순서가 들어간 단어
         """
 
-        for idx, x in enumerate(list(self.tokenize_mecab_compound())):
+        for idx, x in enumerate(list(self.tokenize_mecab_compound(sentence=sentence))):
             copy_x = copy.deepcopy(x)
             copy_x[1].mecab_token_compound_idx = idx
             yield copy_x
 
-    def get_word_from_mecab_compound(self, is_list=False):
+    def get_word_from_mecab_compound(self, sentence: str, is_list=False):
 
         """
         메캅으로 분해된 문장에서 단어만 추출
+        :param sentence: 분석하고 싶은 문장
         :param is_list: 리스트로 반환 여부
         :return: 메캅으로 분해된 문장에서 단어만 포함된 문장
         """
 
         if is_list:
-            return [x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature())]
+            return [x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature(sentence=sentence))]
 
-        return " ".join([x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature())])
+        return " ".join([x[self.FIRST_WORD] for x in list(self.gen_mecab_compound_token_feature(sentence=sentence))])
 
 
 if __name__ == "__main__":
@@ -178,13 +177,13 @@ if __name__ == "__main__":
         test_sentence = "나는 서울대병원에 갔어"
 
         mecab_parse_results = list(
-            MecabParser(test_sentence).gen_mecab_token_feature())
+            MecabParser().gen_mecab_token_feature(test_sentence))
 
         for idx, mecab_parse_item in enumerate(mecab_parse_results):
             print(mecab_parse_item)
 
         mecab_parse_results = list(
-            MecabParser(test_sentence).gen_mecab_compound_token_feature())
+            MecabParser().gen_mecab_compound_token_feature(test_sentence))
 
         for idx, mecab_parse_item in enumerate(mecab_parse_results):
             print(mecab_parse_item)
