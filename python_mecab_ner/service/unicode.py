9dc923472fbf9043d1b97a93a73122675c388dc6
youngchan <san0558@naver.com>
2022-04-24 21:34:00 +0900

    Add to_jaso in unicode.py
    - 문자열 자소로 분해하는 함수 추가

--- python_mecab_ner/service/unicode.py
+++ python_mecab_ner/service/unicode.py
@@ -28,6 +28,23 @@ CHAR_LISTS = {
         0x314c, 0x314d, 0x314e
     ]))
 }
+
+
+NO_JONGSUNG = 'ᴕ'
+
+CHOSUNGS = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+JOONGSUNGS = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+JONGSUNGS = [NO_JONGSUNG, 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ',
+             'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+N_CHOSUNGS = 19
+N_JOONGSUNGS = 21
+N_JONGSUNGS = 28
+
+FIRST_HANGUL = 0xAC00  # '가'
+LAST_HANGUL = 0xD7A3  # '힣'
+
+
 CHAR_INITIALS = CHAR_LISTS[INITIAL]
 CHAR_MEDIALS = CHAR_LISTS[MEDIAL]
 CHAR_FINALS = CHAR_LISTS[FINAL]
@@ -266,3 +283,23 @@ def join_jamos(s, ignore_err=True):
     if queue:
         new_string += flush()
     return new_string
+
+
+def to_jaso(s):
+    result = []
+    for c in s:
+        if ord(c) < FIRST_HANGUL or ord(c) > LAST_HANGUL:  # if a character is a hangul
+            result.append(c)
+        else:
+            code = ord(c) - FIRST_HANGUL
+            jongsung_index = code % N_JONGSUNGS
+            code //= N_JONGSUNGS
+            joongsung_index = code % N_JOONGSUNGS
+            code //= N_JOONGSUNGS
+            chosung_index = code
+
+            result.append(CHOSUNGS[chosung_index])
+            result.append(JOONGSUNGS[joongsung_index])
+            result.append(JONGSUNGS[jongsung_index])
+
+    return ''.join(result)
