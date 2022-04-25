03d21ca659184c8958a2e9ff4c36a82c68c4a3d1
youngchan <san0558@naver.com>
2022-04-25 21:54:10 +0900

    Update test_set_bio_category in test_category_save.py
    - 라벨별 데이터 삽입 테스트 코드 추가

--- tests/test_category_save.py
+++ tests/test_category_save.py
@@ -1,4 +1,6 @@
-from domain.mecab_domain import MecabWordFeature
+from collections import defaultdict
+
+from domain.mecab_domain import MecabTokenStorage
 from service.mecab_category_storage import CategorySave, get_only_entity
 
 
@@ -59,9 +61,9 @@ def test_set_bio_category():
         all_form = f"<{ne_item}:{ne_label}:{ne_begin}-{ne_end}>"
         ne_all_list.append(all_form)
         category_save.set_bi_tag(ne_item, ne_label, ne_begin=ne_begin, ne_end=ne_end)
-        token_all_info.append(category_save.token_info)
+        token_all_info.append((ne_label, category_save.token_info))
 
-    assert token_all_info == [[('훙레이', MecabWordFeature(word='훙레이', pos='NNP', semantic='인명', has_jongseong=False, reading='훙레이', type=None, start_pos=None, end_pos=None, expression=None, space=0, mecab_token=0, mecab_compound=0, begin=0, end=3, label='B-PS_NAME'))], [('대변', MecabWordFeature(word='대변', pos='NNG', semantic=None, has_jongseong=True, reading='대변인', type='Compound', start_pos=None, end_pos=None, expression='대변/NNG/*+인/NNG/*', space=1, mecab_token=1, mecab_compound=1, begin=4, end=6, label='B-CV_POSITION')), ('인', MecabWordFeature(word='인', pos='NNG', semantic=None, has_jongseong=True, reading='대변인', type='Compound', start_pos=None, end_pos=None, expression='대변/NNG/*+인/NNG/*', space=1, mecab_token=1, mecab_compound=2, begin=6, end=7, label='I-CV_POSITION'))], [('한', MecabWordFeature(word='한', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=2, mecab_token=4, mecab_compound=5, begin=10, end=11, label='B-LCG_BAY')), ('반도', MecabWordFeature(word='반도', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=2, mecab_token=4, mecab_compound=6, begin=11, end=13, label='I-LCG_BAY'))], [('한', MecabWordFeature(word='한', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=6, mecab_token=11, mecab_compound=13, begin=26, end=27, label='B-LCG_BAY')), ('반도', MecabWordFeature(word='반도', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=6, mecab_token=11, mecab_compound=14, begin=27, end=29, label='I-LCG_BAY'))], [('비핵', MecabWordFeature(word='핵', pos='NNG', semantic=None, has_jongseong=False, reading='비핵화', type='Compound', start_pos=None, end_pos=None, expression='비핵/NNG/*+화/NNG/*', space=7, mecab_token=12, mecab_compound=15, begin=30, end=32, label='B-AF_WEAPON'))]]
+    assert token_all_info[0][0] == 'PS_NAME'
 
     assert ne_all_list == ['<훙레이:PS_NAME:0-3>', '<대변인:CV_POSITION:4-7>', '<한반도:LCG_BAY:10-13>', '<한반도:LCG_BAY:26-29>', '<핵:AF_WEAPON:31-32>']
 
@@ -79,3 +81,8 @@ def test_set_bio_category():
 
     assert total_length == 5
     assert diff_length == 1
+
+    mecab_token_storage = defaultdict(MecabTokenStorage)
+    category_save.set_mecab_token_storage(mecab_token_storage, token_all_info)
+
+    assert mecab_token_storage['PS_NAME'].core_pos_word.most_common()[0] == (('훙레이', 'NNP'), 1)
