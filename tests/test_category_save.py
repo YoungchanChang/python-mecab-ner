81ec9bf4f55d8935e5bcfbc8c59338f12b23f27f
youngchan <san0558@naver.com>
2022-04-25 20:14:05 +0900

    Update test_set_bio_category in mecab_category_storage.py
    - 토큰별 정보 추가 검증하는 로직 추가

--- tests/test_category_save.py
+++ tests/test_category_save.py
@@ -1,3 +1,4 @@
+from domain.mecab_domain import MecabWordFeature
 from service.mecab_category_storage import CategorySave, get_only_entity
 
 
@@ -49,6 +50,7 @@ def test_set_bio_category():
     assert category_tokens == ['훙레이', '대변', '인', '은', '“', '한', '반도', '평화', '와', '안정', '을', '지키', '고', '한', '반도', '비핵', '화', '를', '추진', '하', '는', '것', '이', '공통', '의', '책임', '과', '의무', '”', '이', '라고', '말', '하', '았', '다', '.']
 
     ne_all_list = []
+    token_all_info = []
     for json_document_ne in all_document_ne:
         ne_label = json_document_ne['label']
         ne_item = json_document_ne['form']
@@ -57,6 +59,9 @@ def test_set_bio_category():
         all_form = f"<{ne_item}:{ne_label}:{ne_begin}-{ne_end}>"
         ne_all_list.append(all_form)
         category_save.set_bi_tag(ne_item, ne_label, ne_begin=ne_begin, ne_end=ne_end)
+        token_all_info.append(category_save.token_info)
+
+    assert token_all_info == [[('훙레이', MecabWordFeature(word='훙레이', pos='NNP', semantic='인명', has_jongseong=False, reading='훙레이', type=None, start_pos=None, end_pos=None, expression=None, space=0, mecab_token=0, mecab_compound=0, begin=0, end=3, label='B-PS_NAME'))], [('대변', MecabWordFeature(word='대변', pos='NNG', semantic=None, has_jongseong=True, reading='대변인', type='Compound', start_pos=None, end_pos=None, expression='대변/NNG/*+인/NNG/*', space=1, mecab_token=1, mecab_compound=1, begin=4, end=6, label='B-CV_POSITION')), ('인', MecabWordFeature(word='인', pos='NNG', semantic=None, has_jongseong=True, reading='대변인', type='Compound', start_pos=None, end_pos=None, expression='대변/NNG/*+인/NNG/*', space=1, mecab_token=1, mecab_compound=2, begin=6, end=7, label='I-CV_POSITION'))], [('한', MecabWordFeature(word='한', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=2, mecab_token=4, mecab_compound=5, begin=10, end=11, label='B-LCG_BAY')), ('반도', MecabWordFeature(word='반도', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=2, mecab_token=4, mecab_compound=6, begin=11, end=13, label='I-LCG_BAY'))], [('한', MecabWordFeature(word='한', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=6, mecab_token=11, mecab_compound=13, begin=26, end=27, label='B-LCG_BAY')), ('반도', MecabWordFeature(word='반도', pos='NNG', semantic='지명', has_jongseong=False, reading='한반도', type='Compound', start_pos=None, end_pos=None, expression='한/NNG/*+반도/NNG/*', space=6, mecab_token=11, mecab_compound=14, begin=27, end=29, label='I-LCG_BAY'))], [('비핵', MecabWordFeature(word='핵', pos='NNG', semantic=None, has_jongseong=False, reading='비핵화', type='Compound', start_pos=None, end_pos=None, expression='비핵/NNG/*+화/NNG/*', space=7, mecab_token=12, mecab_compound=15, begin=30, end=32, label='B-AF_WEAPON'))]]
 
     assert ne_all_list == ['<훙레이:PS_NAME:0-3>', '<대변인:CV_POSITION:4-7>', '<한반도:LCG_BAY:10-13>', '<한반도:LCG_BAY:26-29>', '<핵:AF_WEAPON:31-32>']
 
